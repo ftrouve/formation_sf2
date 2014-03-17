bcd03c500d3189e9e8798e1ad4c741726e2ed674
formation <formation@PP-00057.(none)>
2014-03-17 16:58:16 +0100

    Suppression Acme

--- src/Trouve/BlogBundle/Controller/DefaultController.php
+++ src/Trouve/BlogBundle/Controller/DefaultController.php
@@ -3,11 +3,16 @@
 namespace Trouve\BlogBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 class DefaultController extends Controller
 {
+    /**
+     * @Template("TrouveBlogBundle:Default:index.html.twig")
+     */
     public function indexAction($name)
     {
-        return $this->render('TrouveBlogBundle:Default:index.html.twig', array('name' => $name));
+        return array('name' => $name);
+	//return $this->redirect('http://google.fr');
     }
 }
